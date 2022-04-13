80799ccb5c0cb6144aed540628100b744b6ecbac
zuriyaAnsbacher <zuriya.an@gmail.com>
2022-04-13 12:42:51 +0300

    change run from locally

--- server.py
+++ server.py
@@ -319,5 +319,5 @@ def add_header(r):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    # app.run(debug=True, host='0.0.0.0')
+    # app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
